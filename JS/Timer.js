a72e7dcba8cef2e58d037c4ec1f286c781c1b388
dever <alsrbtkfkd7@gmail.com>
2018-10-09 22:49:08 +0900

    10/09 use localstorage

--- JS/Timer.js
+++ JS/Timer.js
@@ -1,15 +1,16 @@
-let hour = document.getElementById('hour');
-let min = document.getElementById('min');
-let sec = document.getElementById('sec');
-let Form = document.getElementById('SetForm');
-let innerForm = document.getElementById('setForm_in');
+const hour = document.getElementById('hour');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+const Form = document.getElementById('SetForm');
+const innerForm = document.getElementById('setForm_in');
 let time = 0;
 let TimerStart;
-let timerSet = document.getElementById('timer');
-let secondBtns = document.getElementById('secondBtn');
-let submitBtn = document.getElementById('SetFormBtn');
-let saveBtn = document.getElementById('Save');
-let reBtn = document.getElementById('rePlay');
+const secondBtns = document.getElementById('secondBtn');
+const timerSet = document.getElementById('timer');
+const submitBtn = document.getElementById('SetFormBtn');
+const saveBtn = document.getElementById('Save');
+const reBtn = document.getElementById('rePlay');
+const clearBtn = document.getElementById('clear');
 
 // Form
 
@@ -18,7 +19,20 @@ window.onload = function () {
   sec.addEventListener('keydown', GoNextKey);
   saveBtn.addEventListener('click', saveTime);
   reBtn.addEventListener('click', rePlay);
+  clearBtn.addEventListener('click', clear);
+  if(localStorage.time != '') {
+    hour.value = Math.floor(localStorage.time / 3600);
+    min.value = Math.floor((localStorage.time - hour.value * 3600) / 60);
+    sec.value = localStorage.time - hour.value * 3600 - min.value * 60;
+  }
+  else {
+    hour.value = 0;
+    min.value = 0;
+    sec.value = 0;
+  }
 };
+
+
 
 const GoNext = () => {
   submitBtn.style.display = 'none';
@@ -67,6 +81,8 @@ const saveTime = () => {
   saveBtn.style.display = 'none';
   reBtn.style.display = 'flex';
   timerSet.innerHTML = `${hour.value} : ${min.value} : ${sec.value}`;
+  localStorage.setItem('time', time);
+  console.log(localStorage.time);
 };
 
 const rePlay = () => {
@@ -75,3 +91,14 @@ const rePlay = () => {
   time = parseInt(hour.value * 3600) + parseInt(min.value * 60) + parseInt(sec.value);
   TimerStart = setInterval('timer()', 1000);
 };
+
+const clear = () => {
+  localStorage.clear();
+}
+
+// (() => {
+//   submitBtn.addEventListener('click', GoNext);
+//   sec.addEventListener('keydown', GoNextKey);
+//   saveBtn.addEventListener('click', saveTime);
+//   reBtn.addEventListener('click', rePlay);
+// })();
