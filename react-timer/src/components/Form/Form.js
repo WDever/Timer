9d39defdafefaedbcccb74e3efa9e26b351a6cdd
WDever <alsrbtkfkd7@gmail.com>
2018-11-22 22:34:14 +0900

    add packages

---
+++ react-timer/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './From.scss';
+
+const Form = () => {
+  return (
+    <input type="number" placeholder="test" />
+  );
+}
+
+export default Form;
