efae06650e42050f9de86f41f8323013ae3fc194
WDever <alsrbtkfkd7@gmail.com>
2018-11-30 15:07:31 +0900

    Add button , input and styled little

--- react-timer/src/components/Form/Form.js
+++ react-timer/src/components/Form/Form.js
@@ -2,9 +2,30 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Form.scss';
 
+const cx = classNames.bind(styles);
+
 const Form = () => {
   return (
-    <input type="number" placeholder="test" />
+    <div className={cx('form-wrapper')}>
+      <div>
+        <input
+          type="number"
+          placeholder="test"
+          name='hours'
+        />
+        <input
+          type="number"
+          placeholder="test"
+          name='minuates'
+        />
+        <input
+          type="number"
+          placeholder="test"
+          name='second'
+        />
+      </div>
+      <div className={cx('button')}>버튼</div>
+    </div>
   );
 }
 
