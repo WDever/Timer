a77ad130a2531454299ec86d8ea9b0f9b9c6d14d
WDever <alsrbtkfkd7@gmail.com>
2018-12-27 14:58:51 +0900

    inpuuts readonly work

--- react-timer/src/App.js
+++ react-timer/src/App.js
@@ -5,22 +5,58 @@ import Button from './components/Button';
 
 class App extends Component {
   state = {
-    contents: true
+    contents: true,
+    hour: 0,
+    min: 0,
+    sec: 0,
+    time: 0
   };
 
   handleAction = () => {
     const { contents } = this.state;
+
+    console.log(this.state.hour);
+    console.log(this.state.min);
+    console.log(this.state.sec);
 
     this.setState({
       contents: !contents
     });
   };
 
+  handleChange = (e) => {
+    const { value } = e.target;
+
+    this.setState({
+      [e.target.name]: value
+    });
+  }
+
   render() {
-    const { contents } = this.state;
-    const { handleAction } = this;
+    const {
+      contents,
+      hour,
+      min,
+      sec,
+      time
+    } = this.state
+    const {
+      handleAction,
+      handleChange
+    } = this;
+
     return (
-      <TimerTemplate form={<Form />}>
+      <TimerTemplate form={(
+        <Form
+          onChange={handleChange}
+          hour={hour}
+          min={min}
+          sec={sec}
+          value={time}
+          contents={contents}
+        />
+)}
+      >
         <Button
           onClick={handleAction}
           contents={contents}
--- react-timer/src/components/Form/Form.js
+++ react-timer/src/components/Form/Form.js
@@ -4,27 +4,37 @@ import styles from './Form.scss';
 
 const cx = classNames.bind(styles);
 
-const Form = ({ onChange, value }) => {
+const Form = ({ onChange, value, hour, min, sec, contents }) => {
+  const readAble = !contents;
   return (
     <div className={cx('form-wrapper')}>
       <div className={cx('inputs-wrapper')}>
         <input
+          value={hour}
+          onChange={onChange}
           type="number"
           placeholder="00"
-          name='hours'
+          name="hour"
           className={cx('inputs')}
+          readOnly={readAble}
         /> :
         <input
+          value={min}
+          onChange={onChange}
           type="number"
           placeholder="00"
-          name='minuates'
+          name="min"
           className={cx('inputs')}
+          readOnly={readAble}
         /> :
         <input
+          value={sec}
+          onChange={onChange}
           type="number"
           placeholder="00"
-          name='second'
+          name="sec"
           className={cx('inputs')}
+          readOnly={readAble}
         />
       </div>
     </div>
